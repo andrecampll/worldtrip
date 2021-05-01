a1f233a86ce00d42a3890f622493f8b22c85fb9c
André <andrevictor50@gmail.com>
2021-05-01 12:41:07 -0300

    Adding Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+import { Flex, Image } from "@chakra-ui/react";
+
+export function Header() {
+  return (
+    <Flex align="center" justify="center" pt="4">
+      <Image src='/logo.png' />
+    </Flex>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,6 @@
-import { Heading } from "@chakra-ui/layout";
-
 export default function Home() {
   return (
     <div>
-      <Heading>WorldTrip</Heading>
     </div>
   )
 }
