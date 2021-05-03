994ad12dfcb30a97da4173b559923e35276922be
André <andrevictor50@gmail.com>
2021-05-03 06:36:54 -0300

    Adding Mirage, Axios and abstracting Slider component

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import { Box, Flex, Heading, Text } from '@chakra-ui/react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 
@@ -6,13 +7,34 @@ import SwiperCore, {
   Pagination,
 } from 'swiper/core';
 
+import { api } from '../services/api';
+
 import 'swiper/swiper.min.css';
 import 'swiper/components/navigation/navigation.min.css';
 import "swiper/components/pagination/pagination.min.css";
 
 SwiperCore.use([Navigation, Pagination]);
 
+type Continent = {
+  id: number;
+  name: string;
+  description: string;
+  image_url: string;
+}
+
 export function Slider() {
+  const [ continents, setContinents ] = useState<Continent[]>([]);
+
+  useEffect(() => {
+    async function loadContinents() {
+      const { data } = await api.get('/continents');
+
+      setContinents(data);
+    };
+
+    loadContinents();
+  }, []);
+
   return (
     <Box h="450">
       <Swiper
@@ -23,75 +45,33 @@ export function Slider() {
         height: '450px',
         maxWidth: '1240px',
       }}>
-        <SwiperSlide>
-          <Flex
-            backgroundImage="url(https://images.unsplash.com/photo-1490642914619-7955a3fd483c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1070&q=80)"
-            backgroundPosition="center"
-            backgroundRepeat="no-repeat"
-            backgroundSize="cover"
-            height="100%"
-            align="center"
-            justify="center"
-            direction="column"
-          >
-            <Heading as="h4" fontSize="40">
-              Europa
-            </Heading>
-            <Text
-              as="span"
-              fontSize="20"
-              mt="5"
-            >
-              Lorem ipsum dolor sit amet.
-            </Text>
-          </Flex>
-        </SwiperSlide>
-        <SwiperSlide>
-          <Flex
-            backgroundImage="url(https://images.unsplash.com/photo-1540959733332-eab4deabeeaf?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1071&q=80)"
-            backgroundPosition="center"
-            backgroundRepeat="no-repeat"
-            backgroundSize="cover"
-            height="100%"
-            align="center"
-            justify="center"
-            direction="column"
-          >
-            <Heading as="h4" fontSize="40">
-              Ásia
-            </Heading>
-            <Text
-              as="span"
-              fontSize="20"
-              mt="5"
-            >
-              Lorem ipsum dolor sit amet.
-            </Text>
-          </Flex>
-        </SwiperSlide>
-        <SwiperSlide>
-          <Flex
-            backgroundImage="url(https://images.unsplash.com/photo-1516306580123-e6e52b1b7b5f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1098&q=80)"
-            backgroundPosition="center"
-            backgroundRepeat="no-repeat"
-            backgroundSize="cover"
-            height="100%"
-            align="center"
-            justify="center"
-            direction="column"
-          >
-            <Heading as="h4" fontSize="40">
-              América do Sul
-            </Heading>
-            <Text
-              as="span"
-              fontSize="20"
-              mt="5"
-            >
-              Lorem ipsum dolor sit amet.
-            </Text>
-          </Flex>
-        </SwiperSlide>
+        {
+          continents.map(continent => (
+            <SwiperSlide key={continent.id}>
+              <Flex
+                backgroundImage={`url(${continent.image_url})`}
+                backgroundPosition="center"
+                backgroundRepeat="no-repeat"
+                backgroundSize="cover"
+                height="100%"
+                align="center"
+                justify="center"
+                direction="column"
+              >
+                <Heading as="h4" fontSize="40">
+                  {continent.name}
+                </Heading>
+                <Text
+                  as="span"
+                  fontSize="20"
+                  mt="5"
+                >
+                  {continent.description}
+                </Text>
+              </Flex>
+            </SwiperSlide>
+          ))
+        }
       </Swiper>
     </Box>
   )
